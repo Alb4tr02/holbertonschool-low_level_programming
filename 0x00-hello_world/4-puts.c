912d4ba20ffde95928b0fe5db18d16de7b435783
Alb4Tr02 <834@holbertonschool.com>
2019-06-19 11:13:35 -0400

    4-puts.c update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * main - This only prints a sentence
+ *
+ * Return: 0 if all is ok
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
