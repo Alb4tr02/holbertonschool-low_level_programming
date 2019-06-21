5ee880647300b49e79a65f3980cca24540909fbb
Alb4Tr02 <834@holbertonschool.com>
2019-06-21 00:00:20 -0400

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - This only prints the last digit of n
+ *
+ * Return: 0 if all is ok
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 48;
+	for (i = 48; i < 58; i++)
+	{
+		j = i + 1;
+		for (; j < 58; j++)
+		{
+			k = j + 1;
+			for (; k < 58; k++)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (!((j == '7') && (j == '8') && (k == '9')))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
