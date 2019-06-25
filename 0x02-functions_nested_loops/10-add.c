f6e7e77bade9aa851e17cad9cdf8a4f3bb7c1a13
Alb4Tr02 <834@holbertonschool.com>
2019-06-25 10:53:22 -0400

    10-add.c update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ * @a: The number to sum
+ * @b: The number to sum
+ * Return: result of the sum
+ */
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+	return (res);
+}
