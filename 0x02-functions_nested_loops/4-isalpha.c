ff71f2c5277cd3616fb74bb6f5c12ae1a269e529
Alb4Tr02 <834@holbertonschool.com>
2019-06-25 10:23:12 -0400

    4-isalpha.c update

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * _isalpha - This only prints the alphabetin lowercase
+ * @c: The character to test
+ * Return: 1 if is lower
+ *         0 in otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (((c >= 97) && (c <= 122))||((c >=65) && (c<=90)))
+		return (1);
+	else
+		return (0);
+}
