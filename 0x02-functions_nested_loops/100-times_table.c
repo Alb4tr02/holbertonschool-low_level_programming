3ad9fe662d79b388756fade69331ca6ab376781e
Alb4Tr02 <834@holbertonschool.com>
2019-06-25 09:59:04 -0400

    100-times_table.c fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include "holberton.h"
+#include <stdio.h>
 /**
  * print_times_table - function that prints the n times table, starting with 0.
  * @n: number of the table.
@@ -8,7 +9,7 @@
 
 void print_times_table(int n)
 {
-	int res, i, j, aux, aux1, a, t;
+	int res, i, j, aux, aux1, t, aux2;
 
 	if ((n >= 0) && (n <= 15))
 	{
@@ -16,15 +17,16 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				t = 0;
 				res = i * j;
-				aux = res;
 				if (j != 0)
 					_putchar(' ');
 				if ((res < 100) && (j != 0))
 					_putchar(' ');
 				if ((res < 10) && (j != 0))
 					_putchar(' ');
+				t = 0;
+				aux = res;
+				aux2 = 10;
 				while (aux != 0)
 				{
 					aux = aux / 10;
@@ -32,16 +34,15 @@ void print_times_table(int n)
 				}
 				for (; t > 1; t--)
 				{
-					for (aux1 = t - 3; aux1 >= 0; aux1--)
-						a = 10 * a;
-					aux = aux1 < 0 ? (res / 10) : (res / a);
-					res = aux1 < 0 ? (res % 10) : (res % a);
+					aux1 = t - 3;
+					for (; aux1 >= 0; aux1--)
+						aux2 = 10 * aux2;
+					aux = res / aux2;
+					res = res % aux2;
 					_putchar(aux + '0');
-					a = 10;
+					aux2 = 10;
 				}
 				_putchar(res + '0');
-				if (j != n)
-					_putchar(',');
 			}
 			_putchar('\n');
 		}
