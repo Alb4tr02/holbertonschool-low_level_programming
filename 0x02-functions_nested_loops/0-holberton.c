8d8209fca2e8223a6340c596035b5eebd27b4aa3
Alb4Tr02 <834@holbertonschool.com>
2019-06-25 10:08:08 -0400

    0-holberton.c update

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * main - This only prints the alphabet
+ *
+ * Return: 0 if all is ok
+ */
+
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
