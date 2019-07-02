785bacb090ef3c7441c512139d753cf605040d8e
Alb4Tr02 <834@holbertonschool.com>
2019-07-02 12:17:16 -0400

    8-print_array.c update

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - This fuction only prints half of a string
+ * @a:  is the pointer to the array
+ * @n: n is the number of integers
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int aux = 0;
+
+	for (; aux < n; aux++)
+	{
+		if(aux != n - 1)
+			printf("%d, ", *(a + aux));
+		else
+			printf("%d", *(a + aux));
+	}
+	printf("\n");
+}
