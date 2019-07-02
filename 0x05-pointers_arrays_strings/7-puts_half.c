0d6f1d8a32fd7ba92cd39cc9bf77ec0fa51b8a35
Alb4Tr02 <834@holbertonschool.com>
2019-07-02 10:33:47 -0400

    7-puts_half.c update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * puts_half - This fuction only prints half of a string
+ * @str:  is the pointer to the string
+ *
+ */
+
+void puts_half(char *str)
+{
+	int aux = 0;
+	int aux2 = 0;
+
+	for (; *(str + aux) != '\0'; aux++)
+		;
+	aux--;
+	aux2 = aux / 2;
+	if (aux % 2 != 0)
+		aux2++;
+	for (; aux2 <= aux; aux2++)
+		_putchar(*(str + aux2));
+	_putchar('\n');
+}
