e647c43416ee9a7f56e8147584db728ddcd9a553
Alb4Tr02 <834@holbertonschool.com>
2019-07-04 12:12:15 -0400

    3-strcmp.c update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-       	int a = 0;
+	int a = 0;
 	int aux = 0;
 
-        while (*(s1 + a) != '\0' && *(s2 + a) != '\0')
-      	{
+	while (*(s1 + a) != '\0' && *(s2 + a) != '\0')
+	{
 		aux = *(s1 + a) - *(s2 + a);
-		if ( aux != 0)
+		if (aux != 0)
 			return (aux);
 		a++;
 	}
-        return (*(s1+ a) - *(s2 + a));
+	return (*(s1 + a) - *(s2 + a));
 }
