2b1dea18314479bc150300b9fc9f0ab2ddf442c9
Alb4Tr02 <834@holbertonschool.com>
2019-07-04 12:07:30 -0400

    1-strncat.c update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,16 +11,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int aux = 0;
+	int aux = 0;
 	int aux1 = 0;
 
-        for (; *(dest + aux) != '\0'; aux++)
-                ;
+	for (; *(dest + aux) != '\0'; aux++)
+		;
 	while (*(src + aux1) != '\0' && aux1 < n)
-      	{
+	{
 		*(dest + aux) = *(src + aux1);
 		aux1++;
 		aux++;
 	}
-        return (dest);
+	return (dest);
 }
