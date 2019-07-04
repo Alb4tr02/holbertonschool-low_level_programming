f11dcaf65fc55171f48e0f7117b07288745d7f48
Alb4Tr02 <834@holbertonschool.com>
2019-07-04 13:03:48 -0400

    6-cap_string.c update

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * cap_string - This fuction only changes all lowercase letters
+ *                   of a string to uppercase.
+ * @s:  is the pointer to the array
+ * Return: the poiner to the array
+ */
+
+char *cap_string(char *s)
+{
+	int b = 1;
+	int flag = 0;
+	int i;
+	int arr[13] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+
+	while (*(s + b) != '\0')
+	{
+		for (i = 0; i < 13; i++)
+			flag = (s[b - 1] == arr[i]) ? 1 : flag;
+		if (s[b] > 96 && s[b] < 123 && (flag == 1))
+			s[b] = s[b] - 32;
+		flag = 0;
+		b++;
+	}
+	return (s);
+}
