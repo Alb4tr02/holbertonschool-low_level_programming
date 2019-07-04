a3fd45ca305161fcf78b7d3db84dfa8010d5f9d2
Alb4Tr02 <834@holbertonschool.com>
2019-07-04 13:19:18 -0400

    6-cap_string.c fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,17 +9,20 @@
 
 char *cap_string(char *s)
 {
-	int b = 1;
+	int b = 0;
 	int flag = 0;
 	int i;
 	int arr[13] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 
 	while (*(s + b) != '\0')
 	{
-		if (s[b] == 9)
-			s[b] = 32;
+		if (s[0] > 96 && s[0] < 123)
+			s[0] = s[0] - 32;
+		if (b != 0)
+		{
 		for (i = 0; i < 13; i++)
 			flag = (s[b - 1] == arr[i]) ? 1 : flag;
+		}
 		if (s[b] > 96 && s[b] < 123 && (flag == 1))
 			s[b] = s[b] - 32;
 		flag = 0;
