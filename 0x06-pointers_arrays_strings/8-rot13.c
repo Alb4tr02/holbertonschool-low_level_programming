83bb494d1b505bd8c582f6c34430d49c78bb05a3
Alb4Tr02 <834@holbertonschool.com>
2019-07-04 14:20:21 -0400

    8-rot13.c update

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * leet - This fuction only changes all lowercase letters
+ *                   of a string to uppercase.
+ * @s:  is the pointer to the array
+ * Return: the poiner to the array
+ */
+
+char *rot13(char *s)
+{
+	int b = 0;
+	int i = 0;
+	int flag = 1;
+	char *p = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *r = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + b) != '\0')
+	{
+		while (*(p + i) != '\0' && (flag == 1))
+		{
+			if (*(s + b) == *(p + i))
+			{
+				*(s + b) = *(r + i);
+				flag = 0;
+			}
+			i++;
+		}
+		flag = 1;
+		i = 0;
+		b++;
+	}
+	return (s);
+}
