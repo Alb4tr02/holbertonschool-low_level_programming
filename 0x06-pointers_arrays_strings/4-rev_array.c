e24868625b9d120f8ae6358c27e4e1b88877aa4b
Alb4Tr02 <834@holbertonschool.com>
2019-07-04 12:20:22 -0400

    4-rev_array.c update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,13 @@
 
 void reverse_array(int *a, int n)
 {
-       	int b = 0;
+	int b = 0;
 	int aux = 0;
 
-        for (; b < n / 2; b++)
+	for (; b < n / 2; b++)
 	{
-	        aux = *(a + b);
-	        *(a + b) = *(a + (n - b - 1));
+		aux = *(a + b);
+		*(a + b) = *(a + (n - b - 1));
 		*(a + (n - b - 1)) = aux;
 	}
 }
