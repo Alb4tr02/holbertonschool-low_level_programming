e95e783385c2b5bdb85eb39b2d07407e9289319d
Alb4Tr02 <834@holbertonschool.com>
2019-07-08 21:50:57 -0400

    9-set_string.c update

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * set_string - This fuction only sets the value of a pointer to a char
+ * @s:  is the pointer to the section of memory
+ * @to: is the size of the matrix
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
