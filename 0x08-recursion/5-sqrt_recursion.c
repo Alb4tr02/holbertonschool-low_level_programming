35bc82c990e43c79628b8672bf555c6a40bc4360
Alb4Tr02 <834@holbertonschool.com>
2019-07-10 23:03:40 -0400

    5-sqrt_recursion.c chage name of the function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 /**
- * cpow - This fuction only checks if n is perfect square
+ * _cpow - This fuction only checks if n is perfect square
  *        square root of a number.
  * @n:  is the number
  * @i: is the counter
  * Return: natural root square root of n
  */
-
-int cpow(int n, int i)
+int _cpow(int n, int i);
+int _cpow(int n, int i)
 {
 	if (n >= 0)
 	{
-		if (n / 2 >= i)
+		if (n  >= i)
 		{
 			if (i * i == n)
 				return (i);
 			else
-				return (cpow(n, (i + 1)));
+				return (_cpow(n, ++i));
 		}
 	}
 	return (-1);
@@ -25,9 +25,9 @@ int cpow(int n, int i)
  * _sqrt_recursion - This fuction only returns the natural
  *   square root of a number.
  * @n:  is the number
-  * Return: natural root square root of n
+ * Return: natural root square root of n
  */
 int _sqrt_recursion(int n)
 {
-	return (cpow(n, 0));
+	return (_cpow(n, 0));
 }
