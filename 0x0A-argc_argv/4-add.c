144fa5efa513d4468b7a840a1355f0a9684f0ade
Alb4Tr02 <834@holbertonschool.com>
2019-07-14 19:19:37 -0400

    4-add.c update

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - This fuction only print its program name
+ * Return: 0.
+ * @argc:  the number of arguments
+ * @argv: pointer array to the argumments
+ *
+ */
+int  main(int argc, char *argv[])
+{
+	int cont;
+	int res = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		while (--argc)
+		{
+			argv++;
+			cont = 0;
+			while (*(*argv + cont))
+			{
+				if (!isdigit(*(*argv + cont)))
+				{
+					printf("Error\n");
+					return (1);
+				}
+				cont++;
+			}
+			res += atoi(*argv);
+		}
+		printf("%d\n", res);
+	}
+	return (0);
+}
