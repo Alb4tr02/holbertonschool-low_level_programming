525348944c1560f041bf0681a99355367aa52479
Alb4Tr02 <834@holbertonschool.com>
2019-07-16 15:19:40 -0400

    4-free_grid.c fix null grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -11,7 +11,10 @@ void free_grid(int **grid, int height)
 {
 	int k = 0;
 
-	for (; k < height; k++)
-		free(*(grid + k));
-	free(grid);
+	if (grid != 0)
+	{
+		for (; k < height; k++)
+			free(*(grid + k));
+		free(grid);
+	}
 }
