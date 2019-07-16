a3b90ea50448ce8250293984a406d2505b4e9168
Alb4Tr02 <834@holbertonschool.com>
2019-07-16 15:12:40 -0400

    4-free_grid.c update

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * free_grid - function that function that frees a 2 dimensional
+ * grid previously created
+ * to a 2 dimensional array of integers.
+ * @grid: pointer to the matrix
+ * @height: matrix heigth
+ */
+void free_grid(int **grid, int height)
+{
+	int k = 0;
+
+	for (; k < height; k++)
+		free(*(grid + k));
+	free(grid);
+}
