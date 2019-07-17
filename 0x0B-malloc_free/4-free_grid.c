8be35a9a161b266079415ce3d32c8def600e3bd9
Alb4Tr02 <834@holbertonschool.com>
2019-07-16 21:49:07 -0400

    4-free_grid.c fix back to the first version

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,22 +1,18 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "holberton.h"
 /**
- * free_grid -is a function that returns a pointer to a 2 dimensional
- * array of integers.
- * @grid: matrix.
- * @height: heigth of the matrix.
+ * free_grid - function that function that frees a 2 dimensional
+ * grid previously created
+ * to a 2 dimensional array of integers.
+ * @grid: pointer to the matrix
+ * @height: matrix heigth
  */
 void free_grid(int **grid, int height)
 {
-	int i;
+	int k = 0;
 
-	if (grid != 0)
-	{
-		for (i = 0; i < height; i++)
-			free(grid[i]);
-	}
-	else
-	{
-	}
+	for (; k < height; k++)
+		free(*(grid + k));
 	free(grid);
 }
