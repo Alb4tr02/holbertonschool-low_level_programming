15504abf9c9fae91e5a0c5def3e4aebd3876044b
Alb4Tr02 <834@holbertonschool.com>
2019-07-16 22:36:51 -0400

    100-strtow.c fix free

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -69,14 +69,50 @@ void look_pos(char *str, int *pos)
 	}
 }
 /**
+ * print_words - function that stores in m the words found in str
+ * Return: 1 if fail.
+ * @m: pointer to the matrix
+ * @pos: pointer to the array with positions
+ * @words: number of words
+ * @str: pointer to the string
+ */
+
+int print_words(int *pos, char **m, char *str, int words)
+{
+	int b = 0;
+	int b1 = 0;
+	int l = 0;
+
+	for (b = 0; b < words; b++)
+	{
+		int p1 = *(pos + b1);
+		int p2 = *(pos + b1 + 1);
+		int sz = p2 - p1 + 2;
+		*(m + b) = (char *)malloc(sizeof(char) * (sz));
+		if (*(m + b) == NULL)
+		{
+			for (b = b - 1; b >= 0; b--)
+				free(*(m + b));
+			free(m);
+			free(pos);
+			return (1);
+		}
+		for (l = 0; l < sz - 1; l++, p1++)
+			*(*(m + b) + l) = *(str + p1);
+		*(*(m + b) + l) = '\0';
+				b1 = b1 + 2;
+	}
+	*(m + b) = NULL;
+	return (0);
+}
+/**
  * strtow - function that returns a pointer to an array of strings (words)
  * Return: pointer to the array
  * @str: pointer to the array
  */
 char **strtow(char *str)
 {
-	int words, b, l;
-	int b1 = 0;
+	int words;
 	char **m = NULL;
 	int *pos = NULL;
 
@@ -94,27 +130,7 @@ char **strtow(char *str)
 		return (NULL);
 	}
 	look_pos(str, pos);
-
-
-	for (b = 0; b < words; b++)
-	{
-		int p1 = *(pos + b1);
-		int p2 = *(pos + b1 + 1);
-		int sz = p2 - p1 + 2;
-		*(m + b) = (char *)malloc(sizeof(char) * (sz));
-		if (*(m + b) == NULL)
-		{
-			for (b = b - 1; b >= 0; b--)
-				free(*(m + b));
-			free(m);
-			free(pos);
-			return (NULL);
-		}
-		for (l = 0; l < sz - 1; l++, p1++)
-			*(*(m + b) + l) = *(str + p1);
-		*(*(m + b) + l) = '\0';
-				b1 = b1 + 2;
-	}
-	*(m + b) = NULL;
+	if (print_words(pos, m, str, words))
+		return (NULL);
 	return (m);
 }
