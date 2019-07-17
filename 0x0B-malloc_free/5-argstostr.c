ae0ae1193eb67e867834c420b454cee0bc10d8fb
Alb4Tr02 <834@holbertonschool.com>
2019-07-16 22:01:26 -0400

    5-argstostr.c fix

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -5,8 +5,6 @@
  * @ac: size of av.
  * @av: arguments.
  * Return: Returns NULL if ac == 0 or av == NULL
- * Returns a pointer to a new string, or NULL if it fails
- * Each argument should be followed by a \n in the new string
  */
 char *argstostr(int ac, char **av)
 {
