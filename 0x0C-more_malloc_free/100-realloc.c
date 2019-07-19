523ef01c549f93829d118ca386fa33715b9d7f86
Alb4Tr02 <834@holbertonschool.com>
2019-07-18 22:35:50 -0400

    100-realloc.c update fix null ptr

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,6 +22,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	if (ptr == NULL)
+	{
+		newp = malloc(new_size);
+		free(ptr);
+		return (newp);
+	}
 	newp =  malloc(new_size);
 	for (; (a <= new_size) && (b <= old_size); a++, b++)
 		*((char *)newp + a) =  *((char *)ptr + b);
