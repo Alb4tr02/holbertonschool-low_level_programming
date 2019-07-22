a1bd39bc70336ed4260542694cbac071848d0d2c
Alb4Tr02 <834@holbertonschool.com>
2019-07-22 19:45:08 -0400

    4-new_dog.c  update

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - This fuction creates a new dog.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner.
+ * Return: pointer to the new dog.
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *zira;
+	char *n, *o;
+	int a = 0, b = 0;
+
+	zira = malloc(sizeof(dog_t));
+	if (zira == NULL)
+		return (NULL);
+	for (; *(name + a); a++)
+		;
+	for (; *(owner + b); b++)
+		;
+	n = (char *)malloc(sizeof(a));
+	o = (char *)malloc(sizeof(b));
+
+	if (!(a && b))
+		return (NULL);
+	for (; a >= 0; a--)
+		*(n + a) = *(name + a);
+	for (; b >= 0; b--)
+		*(o + a) = *(owner + b);
+	zira->name = n;
+	zira->age = age;
+	zira->owner = o;
+	return (zira);
+}
