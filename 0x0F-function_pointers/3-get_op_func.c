b5ab95df01085104ea3e1238242e3382db3e222d
Alb4Tr02 <834@holbertonschool.com>
2019-07-24 17:48:40 -0400

    3-get_op_func.c fix betty

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,11 @@
 #include "3-calc.h"
 #include <stdlib.h>
+/**
+ * get_op_func - return poiner to the function needed
+ *
+ * @s: poiner to the sign
+ * Return: pointer to the function
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;
