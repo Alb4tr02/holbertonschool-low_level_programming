d6f62a94b2a731e487e1fb6a74dccb64b070b46f
Alb4Tr02 <834@holbertonschool.com>
2019-07-24 13:01:49 -0400

    fix (?)

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,5 @@
-#include <stdlib.h>
-#ifndef NATA_H
-#define NATA_H
+#ifndef NATA
+#define NATA
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
