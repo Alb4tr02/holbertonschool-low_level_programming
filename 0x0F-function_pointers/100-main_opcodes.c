d5d5802054850c02fe684b567dd062d6ee9d2458
Alb4Tr02 <834@holbertonschool.com>
2019-07-24 23:24:13 -0400

    100-main_opcodes.c update

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the main opcode
+ * @argc: number of arguments
+ * @argv: pointer to the arguments
+ * Return: 0 if all is ok
+ */
+int main(int argc, char *argv[])
+{
+	short n = 255;
+	int cont  = 1;
+	int (*ptr_main)(int, char **) = main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	int nb = atoi(*(argv + 1));
+
+	if (nb < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	else
+	{
+		while (nb-- > 1 && cont++)
+			printf("%x ", *((char *)(ptr_main + cont - 2)) & n);
+		printf("%x\n", *((char *)(ptr_main + cont - 1)) & n);
+	}
+	return (0);
+}
