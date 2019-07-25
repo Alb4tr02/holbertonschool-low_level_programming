8c487574d72ae086cf9e3bf10d36e680db6bd4a4
Alb4Tr02 <834@holbertonschool.com>
2019-07-24 22:00:20 -0400

    3-op_functions.c fix betty

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,19 +1,59 @@
+/**
+ * op_add - add two numbers
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: a + b
+ */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - subs two numbers
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: a - b
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * op_mul - multiplicates two numbers
+ *
+ * @a:number 1
+ * @b: number 2
+ * Return: a * b
+ */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - divides two numbers
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: a / b
+ */
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+/**
+ * op_mod - calculates module
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: a % b
+ */
+
 int op_mod(int a, int b)
 {
 	return (a % b);
