9a6453e7193847727476a2d1feed09714de22b35
Alb4Tr02 <834@holbertonschool.com>
2019-07-28 18:51:26 -0400

    variadic_functions.h update

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,19 @@
+#ifndef VARIADIC_FUN
+#define VARIADIC_FUN
+/****************** STRUCTURES ***********************/
+/**
+ * struct _print - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
