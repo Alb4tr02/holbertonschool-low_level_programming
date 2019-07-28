cc9abfc98104abe3268878ca2ad71c52581b9fbb
Alb4Tr02 <834@holbertonschool.com>
2019-07-28 19:14:24 -0400

    2-print_strings.c fix null separator

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,22 +8,34 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	int a = n;
+	unsigned int a = n;
 	va_list args;
 	char *p;
+	char *sep;
 
+	if (separator)
+		sep = (char *)separator;
+	else
+		sep = "";
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
 	va_start(args, n);
 	while (a-- > 1)
 	{
 		p = va_arg(args, char *);
 		if (p)
-			printf("%s%s", p, separator);
+			printf("%s%s", p, sep);
 		else
-			printf("(nil)%s", separator);
+			printf("(nil)%s", sep);
 	}
 	p = va_arg(args, char *);
 	if (va_arg(args, char *))
 		printf("%s\n", p);
 	else
 		printf("(nil)\n");
+	va_end(args);
 }
