fa742b6470df4d9b60a71653a252d58e776e21a3
Alb4Tr02 <834@holbertonschool.com>
2019-07-28 20:37:36 -0400

    100-hello_holberton.asm update

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,14 +1,15 @@
-	section .data
+section .data
 	msg db "Hello, Holberton",10
 
-	section .text
+section .text
 	global main
 main:
-	mov rax,4
-	mov rbx,1
-	mov rcx,msg
-	mov rdx,17
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 17
 	syscall
-	mov rax,1
-	movr rbx,0
+
+	mov rax, 60
+	mov rbx, 0
 	syscall
