d6a95f95bee3a987710c8c3734b3dc21b15f6199
Alb4Tr02 <834@holbertonschool.com>
2019-08-01 14:26:31 -0400

    0-print_list.c fix null pointer

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,24 +8,27 @@
 size_t print_list(const list_t *h)
 {
 
-	list_t *cpyh = h->next;
 	size_t cont = 0;
 
 	if (h)
 	{
+		list_t *cpyh = h->next;
+
 		cont++;
 		if (h->str)
 			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
-	}
-	for (; cpyh != NULL; cont++)
-	{
-		if (cpyh->str)
-			printf("[%u] %s\n", cpyh->len, cpyh->str);
-		else
-			printf("[0] (nil)\n");
-		cpyh = cpyh->next;
+		for (; cpyh != NULL; cont++)
+		{
+			if (cpyh->str)
+				printf("[%u] %s\n", cpyh->len, cpyh->str);
+			else
+				printf("[0] (nil)\n");
+			cpyh = cpyh->next;
+		}
+
+
 	}
 	return (cont);
 }
