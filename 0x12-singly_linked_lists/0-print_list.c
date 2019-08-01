709952841d62934e8e0983252a833f6257624440
Alb4Tr02 <834@holbertonschool.com>
2019-08-01 14:19:37 -0400

    0-print_list.c fix for condition

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * print_list - prints all the elements of a list_t list.
- * @list_t: the list of elements
+ * @h: the list of elements
  * Return: number of the nodes
  */
 size_t print_list(const list_t *h)
@@ -10,18 +10,18 @@ size_t print_list(const list_t *h)
 
 	list_t *cpyh = h->next;
 	size_t cont = 0;
+
 	if (h)
 	{
 		cont++;
 		if (h->str)
-
 			printf("[%u] %s\n", h->len, h->str);
 		else
 			printf("[0] (nil)\n");
 	}
 	else
 		return (0);
-	for (; cpyh->next; cont++)
+	for (; cpyh != NULL; cont++)
 	{
 		if (cpyh->str)
 			printf("[%u] %s\n", cpyh->len, cpyh->str);
