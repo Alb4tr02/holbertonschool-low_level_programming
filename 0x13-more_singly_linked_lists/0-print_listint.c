5324bf841b83ee53af15a53848e1ca1045cc9454
Alb4Tr02 <834@holbertonschool.com>
2019-08-06 10:48:16 -0400

    0-print_listint.c update

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - function that prints all the elements of a listint_t list.
+ * @h: pointer to the first list element
+ *
+ * Return: number of elements printed
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t cont = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		cont++;
+	}
+	return (cont);
+}
