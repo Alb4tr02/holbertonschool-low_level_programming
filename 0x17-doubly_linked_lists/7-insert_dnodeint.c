a360956dacd60df706649906de0e65f1125dd557
Alb4Tr02 <834@holbertonschool.com>
2019-09-15 23:01:23 -0400

    7-insert_dnodeint.c fix limits

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (add_dnodeint(h, n));
 	if (idx > 1)
 		return (NULL);
-	if (idx == 1)
+	if (idx == 1 && cpy)
 	{
 		if (!(cpy->next))
 			return (add_dnodeint_end(h, n));
