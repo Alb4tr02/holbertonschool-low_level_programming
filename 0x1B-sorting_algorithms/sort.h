fcb4f1938f0d76cf1133fc513ef01a12dcd6a3a2
Alb4Tr02 <834@holbertonschool.com>
2019-10-27 22:44:09 -0400

    sort.h fix deck

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -1,5 +1,6 @@
 #ifndef _SORTH_
 #define _SORTH_
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -28,5 +29,4 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 #endif
