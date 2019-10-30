111b0687d4b117036c417eee79c4ebc0bd674856
Alb4Tr02 <834@holbertonschool.com>
2019-10-29 22:12:19 -0400

    1-insertion_sort_list.c fix size == 2

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdio.h>
 /**
  * insertion_sort_list - function that sorts a doubly linked list of integers
  *                      in ascending order using the Insertion sort algorithm
@@ -7,38 +8,38 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *r = NULL;
-	listint_t *l = NULL;
-	listint_t *aux = NULL;
+	listint_t *r, *l, *ar, *al;
 
+	r = l = ar = al = NULL;
 	if (!list || !*list)
 		return;
 	r = *list;
-	l = *list;
 	while (r && r->next)
 	{
 		if (r->n > (r->next)->n)
 		{
-			aux = (r->next)->next;
-			(r->next)->next = r;
-			(r->next)->prev = r->prev;
-			if (r->prev)
-				(r->prev)->next = r->next;
-			r->next = aux;
-			r->prev = (r->prev)->next;
-			if (aux)
-				aux->prev = r;
+			ar = r->next;
+			al = r->prev;
+			if (ar)
+			{
+				ar->prev = al;
+				r->next = ar->next;
+				ar->next = r;
+			}
+			r->prev = ar;
+			if (al)
+				al->next = ar;
+			if (!al && ar)
+				*list = ar;
 			print_list(*list);
 			for (l = r->prev; l && l->prev && (l->n < (l->prev)->n);)
 			{
-				aux = (l->prev)->prev;
-				(l->prev)->prev = l;
-				(l->next)->prev = l->prev;
-				(l->prev)->next = l->next;
-				l->prev = aux;
+				al = (l->prev)->prev, (l->prev)->prev = l;
+				(l->next)->prev = l->prev, (l->prev)->next = l->next;
+				l->prev = al;
 				l->next = (l->next)->prev;
-				if (aux)
-					aux->next = l;
+				if (al)
+					al->next = l;
 				else
 					*list = l;
 				print_list(*list);
