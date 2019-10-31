3177d77e5e5ecb5f2f6b80d7ecbb42db1f1e8e7b
Alb4Tr02 <834@holbertonschool.com>
2019-10-30 21:29:55 -0400

    cuba_libra or mojito ? :v

---
+++ 0x1B-sorting_algorithms/101-cocktail_sort_list.c
@@ -0,0 +1,140 @@
+#include "sort.h"
+/**
+ * shake - function that change two nodes of the list
+ *
+ * @list: Pointer to the head of the list
+ * @i: pointer to the  element
+ * @mode: mode of operation
+ * Return: nothing
+ */
+void shake(listint_t **list, listint_t *i, int mode)
+{
+	listint_t *r;
+	listint_t *l;
+	listint_t *aux;
+
+	r = i->next;
+	l = i->prev;
+	if (mode == 1)
+	{
+		if (l)
+			l->next = r;
+		if (r)
+		{
+			aux = r->next;
+			r->prev = l;
+			r->next = i;
+			i->prev = r;
+			i->next = aux;
+			if (aux)
+				aux->prev = i;
+			if (!(r->prev))
+				*list = r;
+		}
+	}
+	else
+	{
+		if (r)
+			r->prev = l;
+		if (l)
+		{
+			aux = l->prev;
+			l->next = r;
+			l->prev = i;
+			i->prev = aux;
+			i->next = l;
+			if (aux)
+				aux->next = i;
+			if (!(i->prev))
+				*list = i;
+		}
+	}
+}
+/**
+ * cuba_libre - function that sorts a doubly linked list of integers
+ *          in ascending order using the bubble sort algorithm
+ *
+ * @list: Pointer to the head of the list
+ * @start: pointer to the first element
+ * @end: pointer to the last element
+ * Return: null if hte list is sorted
+ */
+listint_t *cuba_libre(listint_t **list, listint_t *start, listint_t *end)
+{
+	listint_t *i;
+	listint_t *flag = NULL;
+	int fl = 1;
+
+	for (i = start; i != end && i->next; )
+	{
+		if (i->n > (i->next)->n)
+		{
+			shake(list, i, 1);
+			fl = 0;
+			print_list(*list);
+		}
+		else
+			i = i->next;
+	}
+	if (!fl)
+		flag = i->prev;
+	return (flag);
+}
+/**
+ * mojito - function that sorts a doubly linked list of integers
+ *          in ascending order using the bubble sort algorithm
+ *
+ * @list: Pointer to the head of the list
+ * @start: pointer to the first element
+ * @end: pointer to the last element
+ * Return: null if hte list is sorted
+ */
+listint_t *mojito(listint_t **list, listint_t *start, listint_t *end)
+{
+	listint_t *i;
+	listint_t *flag = NULL;
+	int fl = 1;
+
+	for (i = start; i != end && i->prev;)
+	{
+		if (i->n < (i->prev)->n)
+		{
+			shake(list, i, 2);
+			fl = 0;
+			print_list(*list);
+		}
+		else
+			i = i->prev;
+	}
+	if (!fl)
+		flag = i->next;
+	return (flag);
+}
+/**
+ * cocktail_sort_list - function that sorts a doubly linked list of integers
+ *               in ascending order using the Cocktail shaker sort algorithm
+ *
+ * @list: Pointer to the first element
+ *
+ * Return: nothing
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *st, *end;
+
+	if (!list || !(*list))
+		return;
+	st = cuba_libre(list, *list, NULL);
+	if (!st)
+		return;
+	end = mojito(list, st, NULL);
+	while (1)
+	{
+		if (!end)
+			return;
+		st = cuba_libre(list, end, st);
+		if (!st)
+			return;
+		end = mojito(list, st, end);
+	}
+}
