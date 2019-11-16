537c28356dcb27205ba1dd1e5eadd686e0af0f5a
Alb4Tr02 <834@holbertonschool.com>
2019-11-15 23:42:05 -0500

    5-island_perimeter.py ninja style

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,9 +9,9 @@ def island_perimeter(grid):
     s, a, b, c = "", 0, 0, len(grid[0])
     for row in grid:
         s, b = s + ''.join([str(elem) for elem in row]), b + 1
-    for i in range(len(s) - 1):
-        a += 1 if (s[i] == '0' and (s[i - 1] == '1' or s[i + 1] == '1')) else 0
     for i in range(c, c*(b - 1), 1):
-        a += 1 if (s[i] == '1' and s[i - c] == '0' and s[i - c - 1] != '1') else 0
+        a += 1 if (s[i] == '1' and s[i - c] == '0') else 0
         a += 1 if (s[i] == '1' and s[i + c] == '0') else 0
+        a += 1 if (s[i] == '1' and s[i - 1] == '0') else 0
+        a += 1 if (s[i] == '1' and s[i + 1] == '0' and s[i + 2] != '1') else 0
     return a
