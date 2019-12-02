68b239e76008a3204cb6f3c9f512dc665318ab4a
Alb4Tr02 <834@holbertonschool.com>
2019-12-02 12:23:51 -0500

    0-binary_tree_node.c update

---
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *bt = malloc(sizeof(binary_tree_t));
+	if (!bt)
+		return (NULL);
+	bt->n = value;
+	bt->parent = parent;
+	return (bt);
+}
