d25f3c9e138d4b6c9c4d425c2328dfa7d012a1bc
Alb4Tr02 <834@holbertonschool.com>
2019-12-02 15:30:26 -0500

    9-binary_tree_height.c update

---
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t r1 = 0;
+	size_t r2 = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left || tree->right)
+	{
+		r1 = binary_tree_height(tree->right);
+		r2 = binary_tree_height(tree->left);
+		return r1 >= r2 ? r1 + 1: r2 + 1;
+	}
+	return 0;
+}
