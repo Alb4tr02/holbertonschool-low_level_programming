f19ac1891508a4187ad8d4e3f883de1636683993
Alb4Tr02 <834@holbertonschool.com>
2019-12-05 17:25:29 -0500

    17-binary_tree_sibling.c betty

--- 0x1D-binary_trees/17-binary_tree_sibling.c
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -1,4 +1,9 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_sibling - function that finds the sibling of a node
+ * @node: pointer to the node
+ * Return: sibling's node
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !(node->parent))
