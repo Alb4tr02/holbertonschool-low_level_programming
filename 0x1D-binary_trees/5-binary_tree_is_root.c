f765b86908024dbe2896a0cc4bab58e76b5a89b8
Alb4Tr02 <834@holbertonschool.com>
2019-12-05 13:04:18 -0500

    5-binary_tree_is_root.c update

--- 0x1D-binary_trees/5-binary_tree_is_root.c
+++ 0x1D-binary_trees/5-binary_tree_is_root.c
@@ -1,4 +1,10 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_is_root - function that checks if a given node is a root
+ *
+ * @node: node to check
+ * Return: 1 if the node is a root
+ */
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (!node)
