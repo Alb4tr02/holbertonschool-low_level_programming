7eeb455a8fda3cc8f9ed35347686a4a73c96ac0b
Alb4Tr02 <834@holbertonschool.com>
2019-12-05 12:59:37 -0500

    3-binary_tree_delete.c update

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -1,6 +1,13 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_delete -  function that deletes an entire binary tree
+ *
+ * @tree: root tree
+ * Return: nothing
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
+
 	if (!tree)
 		return;
 	binary_tree_delete(tree->right);
