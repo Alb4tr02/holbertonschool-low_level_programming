e9b3568285f27cce1dab934c7711b07a6865915d
Alb4Tr02 <834@holbertonschool.com>
2019-12-05 13:02:28 -0500

    4-binary_tree_is_leaf.c betty

--- 0x1D-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -1,4 +1,10 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - function that checks if a node is a leaf
+ *
+ * @node: node to check
+ * Return: 1 if is a leaf
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node)
