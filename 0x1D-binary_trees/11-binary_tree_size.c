c53d9d0720170157d404e8143920c888dd6dedc3
Alb4Tr02 <834@holbertonschool.com>
2019-12-05 13:28:51 -0500

    11-binary_tree_size.c update

--- 0x1D-binary_trees/11-binary_tree_size.c
+++ 0x1D-binary_trees/11-binary_tree_size.c
@@ -1,4 +1,9 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree: pointer to tree
+ * Return: tree's size
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
