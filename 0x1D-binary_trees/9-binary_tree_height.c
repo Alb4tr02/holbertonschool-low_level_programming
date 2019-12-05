90e1207f6f7c26dae87c7336601759d205aa07b9
Alb4Tr02 <834@holbertonschool.com>
2019-12-05 13:23:07 -0500

    9-binary_tree_height.c update

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -1,4 +1,9 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_height -  function that measures the height of a binary tree
+ * @tree: pointer to tree
+ * Return: nothing
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t r1 = 0;
@@ -10,7 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		r1 = binary_tree_height(tree->right);
 		r2 = binary_tree_height(tree->left);
-		return r1 >= r2 ? r1 + 1: r2 + 1;
+		return (r1 >= r2 ? r1 + 1 : r2 + 1);
 	}
-	return 0;
+	return (0);
 }
