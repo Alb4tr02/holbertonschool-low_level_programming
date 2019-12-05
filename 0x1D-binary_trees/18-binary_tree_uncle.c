a60a8287796ad9efc0481a761527c219b1e98baf
Alb4Tr02 <834@holbertonschool.com>
2019-12-05 17:27:40 -0500

    18-binary_tree_uncle.c update

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -1,4 +1,9 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_sibling - function that finds the sibling of a node
+ * @node: pointer to the node
+ * Return: sibling's node
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !(node->parent))
@@ -6,6 +11,11 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	return ((node->parent)->left == node ? node->parent->right :
 		node->parent->left);
 }
+/**
+ * binary_tree_uncle - function that finds the uncle of a node
+ * @node: pointer to the node
+ * Return: uncle's node
+ */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !(node->parent))
