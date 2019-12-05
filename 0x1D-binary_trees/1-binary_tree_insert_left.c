358f057e34f3cafd8e4ee2077cad2d0706ad4596
Alb4Tr02 <834@holbertonschool.com>
2019-12-05 12:54:15 -0500

    1-binary_tree_insert_left.c betty

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -9,10 +9,11 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *bt = binary_tree_node(parent, value);
+	binary_tree_t *bt = NULL;
 
 	if (!parent)
 		return (NULL);
+	bt = binary_tree_node(parent, value);
 	if (!bt)
 		return (NULL);
 	if (parent->left)
