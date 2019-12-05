ea40e0537cffe7d2981feb56ba27b2d6f4398abd
Alb4Tr02 <834@holbertonschool.com>
2019-12-05 12:33:33 -0500

    0-binary_tree_node.c betty

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -1,7 +1,15 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_node - function that creates a binary tree node
+ *
+ * @parent: node's parent
+ * @value: value stored in the node
+ * Return: new node
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *bt = calloc(1, sizeof(binary_tree_t));
+
 	if (!bt)
 		return (NULL);
 	bt->n = value;
