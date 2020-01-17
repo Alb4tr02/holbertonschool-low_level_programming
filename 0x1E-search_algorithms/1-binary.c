35fcfff431f9140d3947a196d34c7d2e21b24d66
Alb4Tr02 <834@holbertonschool.com>
2020-01-16 22:54:46 -0500

    1-binary.c update

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+/**
+ * p -  function that searches for a value in an array of integers
+ *                  using the Binary search algorithm
+ * @array: pointer to the array
+ * @start: number of elements in array
+ * @end: the value to search for
+ * Return: Always EXIT_SUCCESS
+ */
+void p(int *array, size_t start, size_t end)
+{
+	char *sp;
+
+	printf("Searching in array: ");
+	for (sp = ""; start <= end; start++, sp = ", ")
+		printf("%s%d", sp, array[start]);
+	printf("\n");
+}
+/**
+ * binary_search -  function that searches for a value in an array of integers
+ *                  using the Binary search algorithm
+ * @array: pointer to the array
+ * @size: number of elements in array
+ * @value: the value to search for
+ * Return: Always EXIT_SUCCESS
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t m, l, r = 0;
+
+	if (!array)
+		return (-1);
+	for (l = 0, r = size - 1, p(array, l, r), m = (l + r) / 2; l < r;
+	     m = (l + r) / 2, p(array, l, r))
+	{
+		if (value == array[m])
+			return ((int) m);
+		l = (array[m] > value) ? l : m + 1;
+		r = (array[m] > value) ? m - 1 : r;
+	}
+	return (-1);
+}
